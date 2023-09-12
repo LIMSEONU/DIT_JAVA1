310cca92f41eda868ae526159d4c62aaad24e703
LIMSEONU <144203174+LIMSEONU@users.noreply.github.com>
2023-09-12 20:50:36 +0900

    Add files via upload

---
+++ Sorry.java
@@ -0,0 +1,12 @@
+
+public class Sorry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sorry~~");
+		System.out.print("재밌어서 미안합니다~~");
+		
+	}
+
+}
---
+++ Welcome.java
@@ -0,0 +1,13 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome!!");
+		System.out.print("자바 세계로 오신 것을 환영합니다.");
+		
+
+	}
+
+}
